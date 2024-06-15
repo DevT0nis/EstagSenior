aefef2d52e51e71b4bf4c3e7c71042832d5067d8
anthonythom <devtoniscontato@gmail.com>
2024-06-15 00:31:46 -0300

    removendo howl

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
 import styled from "styled-components";
-import { useState, useEffect } from "react";
-import { Howl } from "howler";
+// import { useState, useEffect } from "react";
+// import { Howl } from "howler";
 
 const Container = styled.div`
   display: flex;
@@ -13,76 +13,76 @@ const CanvasContainer = styled.div`
   flex: 1;
 `;
 
-const UIContainer = styled.div`
-  position: absolute;
-  top: 20px;
-  left: 20px;
-  z-index: 10;
-`;
+// const UIContainer = styled.div`
+//   position: absolute;
+//   top: 20px;
+//   left: 20px;
+//   z-index: 10;
+// `;
 
-const Button = styled.button`
-  background-color: #333133;
-  border-radius: 20px;
-  height: 50px;
-  width: 150px;
-  color: #6fff00;
-  border: none;
-  font-family: "Poppins", sans-serif;
-  font-weight: 300;
-  font-style: normal;
-  font-size: 17px;
-  margin: 5px;
-  &:hover {
-    background-color: #131213;
-  }
-`;
+// const Button = styled.button`
+//   background-color: #333133;
+//   border-radius: 20px;
+//   height: 50px;
+//   width: 150px;
+//   color: #6fff00;
+//   border: none;
+//   font-family: "Poppins", sans-serif;
+//   font-weight: 300;
+//   font-style: normal;
+//   font-size: 17px;
+//   margin: 5px;
+//   &:hover {
+//     background-color: #131213;
+//   }
+// `;
 
-const VolumeSlider = styled.input`
-  width: 100%;
-  margin: 10px 0;
-`;
+// const VolumeSlider = styled.input`
+//   width: 100%;
+//   margin: 10px 0;
+// `;
 
 function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [volume, setVolume] = useState(0.5);
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const [volume, setVolume] = useState(0.5);
 
 
-  const sound = new Howl({
-    src: ['./music/Minecraft FULL SOUNDTRACK.mp3'],
-    autoplay: false,
-    loop: false,
-    volume: 0.5, 
-  });
-
- 
-  useEffect(() => {
-    sound.volume(volume);
-  }, [volume]);
-
-
-  const handlePlayPauseMusic = () => {
-    if (isPlaying) {
-      sound.pause();
-    } else {
-      sound.play();
-    }
-    setIsPlaying(!isPlaying); 
-  };
+  // const sound = new Howl({
+  //   src: ['./music/Minecraft FULL SOUNDTRACK.mp3'],
+  //   autoplay: false,
+  //   loop: false,
+  //   volume: 0.5, 
+  // });
 
  
-  const handleVolumeChange = (event) => {
-    setVolume(parseFloat(event.target.value));
-  };
+  // useEffect(() => {
+  //   sound.volume(volume);
+  // }, [volume]);
+
+
+  // const handlePlayPauseMusic = () => {
+  //   if (isPlaying) {
+  //     sound.pause();
+  //   } else {
+  //     sound.play();
+  //   }
+  //   setIsPlaying(!isPlaying); 
+  // };
+
+ 
+  // const handleVolumeChange = (event) => {
+  //   setVolume(parseFloat(event.target.value));
+  // };
 
   return (
     <Container>
       <CanvasContainer>
         <Canvas shadows camera={{ position: [0, 2, 5], fov: 30 }}>
           <color attach="background" args={["#ececec"]} />
-          <Experience setIsPlaying={setIsPlaying} />
+          <Experience />
         </Canvas>
       </CanvasContainer>
-      <UIContainer>
+      {/* <UIContainer>
         <Button onClick={handlePlayPauseMusic}>
           {isPlaying ? "Pausar Música" : "Tocar Música"}
         </Button>
@@ -94,7 +94,7 @@ function App() {
           value={volume}
           onChange={handleVolumeChange}
         />
-      </UIContainer>
+      </UIContainer> */}
     </Container>
   );
 }
